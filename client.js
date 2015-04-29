56d1f38b26cde9f0725649e5434b40fa84f6b0a0
Leo McArdle <leomcardle@gmail.com>
2015-04-29 18:07:08 +0100

    initial commit

---
+++ client.js
@@ -0,0 +1,109 @@
+var PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+var IceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
+var SessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
+
+var sid = getSid();
+var cid = createCid();
+
+var pc;
+var pcConfig = {
+  iceServers: [{ url: 'stun:stun.services.mozilla.com' }]
+}
+var dc;
+
+var fileLength;
+var blobLength = 0;
+var blobArray = [];
+
+var ws = new WebSocket('ws://localhost:8080');
+
+ws.onopen = function(){
+  signallingSend({
+    type: 'file-request',
+    sid: sid,
+    cid: cid
+  });
+}
+
+ws.onmessage = function(dataString){
+  data = JSON.parse(dataString.data);
+  switch(data.type){
+    case 'file-info':
+      onFileInfo(data);
+      break;
+    case 'ice-candidate':
+      onIceCandidate(data);
+      break;
+    case 'webrtc-offer':
+      onWebrtcOffer(data);
+      break;
+  }
+}
+
+function onFileInfo(data){
+  fileLength = data.length;
+  
+  pc = new PeerConnection(pcConfig);
+  
+  pc.onicecandidate = function(evt){
+    if(evt.candidate)
+      signallingSend({
+        type: 'ice-candidate',
+        sid: sid,
+        cid: cid,
+        candidate: evt.candidate
+      });
+  }
+
+  pc.ondatachannel = function(evt){
+    dc = evt.channel;
+    dc.binaryType = 'arraybuffer';
+    dc.onmessage = function(data){
+      blobLength += data.data.byteLength;
+      blobArray.push(data.data);
+      if(blobLength >= fileLength) {
+        window.location = URL.createObjectURL(new Blob(blobArray));
+      }
+    }
+  }
+}
+
+function onIceCandidate(data){
+  pc.addIceCandidate(new IceCandidate(data.candidate), function(){}, onError);
+}
+
+function onWebrtcOffer(data){
+  pc.setRemoteDescription(new SessionDescription(data.offer), function(){
+    pc.createAnswer(function(answer){
+      pc.setLocalDescription(answer, function(){
+        signallingSend({
+          type: 'webrtc-answer',
+          sid: sid,
+          cid: cid,
+          answer: pc.localDescription
+        });
+      }, onError);
+    }, onError);
+  }, onError);
+}
+
+function onError(err){
+  console.log(err);
+}
+
+function signallingSend(json){
+  ws.send(JSON.stringify(json));
+}
+
+function createCid(){
+  return Date.now()+""+Math.floor(Math.random()*1000);
+}
+
+function getSid(){
+  return getParameterByName('q');
+}
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
---
+++ server.js
@@ -0,0 +1,39 @@
+var WebSocketServer = require('ws').Server;
+
+var wss = new WebSocketServer({ port: 8080 });
+
+var server = {};
+var fileInfo = {};
+var client = {};
+
+wss.on('connection', function(ws){
+  ws.on('message', function(dataString){
+    console.log(dataString);
+    data = JSON.parse(dataString);
+    try {
+      switch(data.type){
+        case 'file-info':
+          server[data.sid] = ws;
+          fileInfo[data.sid] = data;
+          break;
+        case 'file-request':
+          client[data.cid] = ws;
+          ws.send(JSON.stringify(fileInfo[data.sid]));
+          server[data.sid].send(dataString);
+          break;
+        case 'ice-candidate':
+          if(ws == server[data.sid]) client[data.cid].send(dataString);
+          else server[data.sid].send(dataString);
+          break;
+        case 'webrtc-offer':
+          client[data.cid].send(dataString);
+          break;
+        case 'webrtc-answer':
+          server[data.sid].send(dataString);
+          break;
+      }
+    } catch(e){
+      console.log(e)
+    }
+  });
+});
