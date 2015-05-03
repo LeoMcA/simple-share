af395885365ce908904e0f58803a53a66a3e7b84
Leo McArdle <leomcardle@gmail.com>
2015-05-03 17:03:41 +0100

    combined client and server in client-side js

--- simple-share.js
+++ simple-share.js
@@ -4,44 +4,74 @@ webrtc.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnecti
 webrtc.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
 webrtc.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
 
-document.getElementById('input').addEventListener('change', function(){
-  var file = this.files[0];
-  reader = new FileReader();
-  reader.onload = function(){
-    fileBuffer = common.setFileBuffer(this.result, this.result.byteLength);
-    createHexHash('SHA-1', fileBuffer, function(hash){
-      var sid = common.setSid(hash);
-      
-      var server = getParameterByName('s');
-      var client = getParameterByName('c');
-      
-      if(server){
-        if(client)
-          console.log(client + '?s=' + server + '&i=' + sid);
-        else
-          console.log('http://' + server + '/client.html?s=' + server + '&i=' + sid);
-        var ws = common.setSignalling(new WebSocket('ws://' + server));
-      } else {
-        console.log('http://simple-share.mca.me.uk/client.html?s=simple-share.mca.me.uk&i=' + sid);
-        var ws = common.setSignalling(new WebSocket('ws://simple-share.mca.me.uk'));
-      }
-      
-      ws.onopen = function(){
-        common.onSignallingConnect();
-      }
-      ws.onmessage = function(data){
-        common.onSignallingMessage(data.data, onDataChannel);
-      }
-      
-    });
-  }
-  reader.readAsArrayBuffer(file);
-}, false);
+if(getSid()){
+  //we're a client
+  common.setClient(true);
+  common.setSid(getSid());
 
-function onDataChannel(dc){
-  console.log('dc open');
-  for(var i = 0; i <= fileBuffer.byteLength; i += 66528){
-    dc.send(fileBuffer.slice(i, i + 66528));
+  var blobLength = 0;
+  var blobArray = [];
+
+  var ws = common.setSignalling(new WebSocket('ws://' + getParameterByName('s')));
+
+  ws.onopen = function(){
+    common.onSignallingConnect();
+  }
+
+  ws.onmessage = function(data){
+    common.onSignallingMessage(data.data, onDataChannel);
+  }
+
+  function onDataChannel(dc){
+    dc.onmessage = function(data){
+      blobLength += data.data.byteLength;
+      blobArray.push(data.data);
+      if(blobLength >= common.fileLength) {
+        window.location = URL.createObjectURL(new Blob(blobArray));
+      }
+    }
+  }
+} else {
+  //we're a server
+  document.getElementById('input').addEventListener('change', function(){
+    var file = this.files[0];
+    reader = new FileReader();
+    reader.onload = function(){
+      fileBuffer = common.setFileBuffer(this.result, this.result.byteLength);
+      createHexHash('SHA-1', fileBuffer, function(hash){
+        var sid = common.setSid(hash);
+
+        var server = getParameterByName('s');
+        var client = getParameterByName('c');
+
+        if(server){
+          if(client)
+            console.log(client + '?s=' + server + '&i=' + sid);
+          else
+            console.log('http://' + server + '/simple-share.html?s=' + server + '&i=' + sid);
+          var ws = common.setSignalling(new WebSocket('ws://' + server));
+        } else {
+          console.log('http://simple-share.mca.me.uk/client.html?s=simple-share.mca.me.uk&i=' + sid);
+          var ws = common.setSignalling(new WebSocket('ws://simple-share.mca.me.uk'));
+        }
+
+        ws.onopen = function(){
+          common.onSignallingConnect();
+        }
+        ws.onmessage = function(data){
+          common.onSignallingMessage(data.data, onDataChannel);
+        }
+
+      });
+    }
+    reader.readAsArrayBuffer(file);
+  }, false);
+
+  function onDataChannel(dc){
+    console.log('dc open');
+    for(var i = 0; i <= fileBuffer.byteLength; i += 66528){
+      dc.send(fileBuffer.slice(i, i + 66528));
+    }
   }
 }
 
@@ -56,6 +86,10 @@ function createHexHash(algo, arrayBuffer, callback){
     });
     callback(hex);
   });
+}
+
+function getSid(){
+  return getParameterByName('i');
 }
 
 function getParameterByName(name) {
